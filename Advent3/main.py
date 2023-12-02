5d84334b336eb4f6e61d38206ed6bfc1a4968b75
Micahel Tezza <mtezza9@gmail.com>
2023-12-02 13:04:37 +0100

    description

---
+++ Advent3/main.py
@@ -0,0 +1,10 @@
+"""
+ADVENT OF CODE 2023 - DAY 3
+Author: Michael Tezza
+Date: 03/12/2023
+Description: 
+    Part 1: 
+        it is useless to look I don't know the future
+    Part 2:
+        it is useless to look I don't know the future
+"""
