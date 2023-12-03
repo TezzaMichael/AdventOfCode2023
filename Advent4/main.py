08c2748ede1cafd62041cde67a0e598e0c8b406c
Micahel Tezza <mtezza9@gmail.com>
2023-12-03 22:28:33 +0100

    description advent4

---
+++ Advent4/main.py
@@ -0,0 +1,10 @@
+"""
+ADVENT OF CODE 2023 - DAY 4
+Author: Michael Tezza
+Date: 04/12/2023
+Description: 
+    Part 1: 
+        BWHOA! I do not foresee the future unfortunately. I will have to come back to this one.
+    Part 2:
+        BWHOA! I do not foresee the future unfortunately. I will have to come back to this one.
+"""
