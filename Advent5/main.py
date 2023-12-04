29bccb492ae0fbba0783dda23d8fea512206796e
Michael Tezza <Michael.Tezza@acs.it>
2023-12-04 14:08:15 +0100

    waiting...

---
+++ Advent5/main.py
@@ -0,0 +1,10 @@
+"""
+ADVENT OF CODE 2023 - DAY 5
+Author: Michael Tezza
+Date: 05/12/2023
+Description: 
+    Part 1: 
+        BWHOA! I do not foresee the future unfortunately. I will have to come back to this one.
+    Part 2:
+        BWHOA! I do not foresee the future unfortunately. I will have to come back to this one.
+"""
